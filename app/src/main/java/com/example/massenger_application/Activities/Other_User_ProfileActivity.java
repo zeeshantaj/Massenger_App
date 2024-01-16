bcb2113998ca6f803423eab6f12ce9f127e96fa6
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-01-16 23:56:51 +0500

    fourth5 commit

--- app/src/main/java/com/example/massenger_application/Activities/Other_User_ProfileActivity.java
+++ app/src/main/java/com/example/massenger_application/Activities/Other_User_ProfileActivity.java
@@ -8,21 +8,36 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.massenger_application.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Other_User_ProfileActivity extends AppCompatActivity {
 
 
     private ImageView otherProfileBackBtn;
+    private CircleImageView profileImg;
+    private TextView name,status,phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_user_profile);
         otherProfileBackBtn = findViewById(R.id.otherProfileBackBtn);
+        profileImg = findViewById(R.id.OtherUserProfileImg);
+        name = findViewById(R.id.otherProfileName);
+        phoneNumber = findViewById(R.id.textView8);
+        status = findViewById(R.id.otherUserStatus);
+
+
         otherProfileBackBtn.setOnClickListener(v -> {
             finish();
         });
+    }
+
+    private void setProfileInfo(){
 
     }
+
 }
