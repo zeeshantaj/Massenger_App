a48ea1c784ce660b52e678499a90c26d12e7c1d5
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-01-16 19:42:43 +0500

    fourth4 commit

--- app/src/main/java/com/example/massenger_application/Activities/Other_User_ProfileActivity.java
+++ app/src/main/java/com/example/massenger_application/Activities/Other_User_ProfileActivity.java
@@ -1,16 +1,28 @@
 package com.example.massenger_application.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.example.massenger_application.R;
 
 public class Other_User_ProfileActivity extends AppCompatActivity {
 
+
+    private ImageView otherProfileBackBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_user_profile);
+        otherProfileBackBtn = findViewById(R.id.otherProfileBackBtn);
+        otherProfileBackBtn.setOnClickListener(v -> {
+            finish();
+        });
+
     }
 }
