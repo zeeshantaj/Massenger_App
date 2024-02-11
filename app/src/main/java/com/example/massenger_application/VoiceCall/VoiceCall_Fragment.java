5e05397214066549f1b329aa47ea8a49e4297458
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-02-11 22:27:08 +0500

    voice call feature commit

--- app/src/main/java/com/example/massenger_application/VoiceCall/VoiceCall_Fragment.java
+++ app/src/main/java/com/example/massenger_application/VoiceCall/VoiceCall_Fragment.java
@@ -9,12 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.massenger_application.R;
+
 public class VoiceCall_Fragment extends Fragment {
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.voice_call_fragment_layout,container,false);
     }
 }
