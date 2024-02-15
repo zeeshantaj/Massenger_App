60fffdf20a5b4eef9617d44bef8118d06d45b49f
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-02-15 19:41:55 +0500

    voice call feature14 commit

--- app/src/main/java/com/example/massenger_application/Chat/ChatActivity.java
+++ app/src/main/java/com/example/massenger_application/Chat/ChatActivity.java
@@ -362,28 +362,28 @@ public class ChatActivity extends AppCompatActivity {
         });
 
     }
-    void callApi(JSONObject jsonObject){
-        MediaType json = MediaType.get("application/json; charset=utf-8");
-        OkHttpClient client = new OkHttpClient();
-        String url = "https://fcm.googleapis.com/fcm/send";
-        RequestBody body = RequestBody.create(jsonObject.toString(),json);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .header("Authorization","Bearer AAAA7reCDoQ:APA91bHKOMdHeoUOtVUznGapVkZe27TYREQXUNAcON5FUepw3w_GOAer_ODZhNCmYC-ihXlagEojfs8dvj3DcWgeJqQpvYbxmRz8FMXWrb_b1keKYtRsv1agAC1fKnyiyJ1WWWKCO65F")
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-
-            }
-        });
-    }
+//    void callApi(JSONObject jsonObject){
+//        MediaType json = MediaType.get("application/json; charset=utf-8");
+//        OkHttpClient client = new OkHttpClient();
+//        String url = "https://fcm.googleapis.com/fcm/send";
+//        RequestBody body = RequestBody.create(jsonObject.toString(),json);
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .post(body)
+//                .header("Authorization","Bearer AAAA7reCDoQ:APA91bHKOMdHeoUOtVUznGapVkZe27TYREQXUNAcON5FUepw3w_GOAer_ODZhNCmYC-ihXlagEojfs8dvj3DcWgeJqQpvYbxmRz8FMXWrb_b1keKYtRsv1agAC1fKnyiyJ1WWWKCO65F")
+//                .build();
+//        client.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+//
+//            }
+//
+//            @Override
+//            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+//
+//            }
+//        });
+//    }
 
     private void CreateChatRoom(){
         //FirebaseAuth auth = FirebaseAuth.getInstance();
